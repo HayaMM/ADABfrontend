bbe7ce44118546e17039a9960f1c8f56e249cabe
Rasha Alkaabi <Rasha.a.1418@htmail.com>
2021-02-22 15:06:50 +0300

    The sign up

--- src/AdabApp.js
+++ src/AdabApp.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Register from "./user/Register";
+import Login from "./user/Login";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class AdabApp extends Component {
+
+  state={
+    isUser: false,
+    user:null,
+    message:null
+  }
+  // method for rigstration
+  registerHandler = (user) => {
+    axios.post("adab/user/registration", user)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  render() {
+    return (
+      <Router>
+        <nav>
+            <div>
+              <Link to="/register">Register</Link> {' '}
+              <Link to="/login">Login</Link> {' '}
+            </div>
+        </nav>
+        <div>
+<Route path="/register" component={() => <Register register={this.registerHandler} />}></Route>
+          <Route path="/login" component={Login}></Route>
+        </div>
+    </Router>
+    )
+  }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AdabApp from './AdabApp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AdabApp />
   </React.StrictMode>,
   document.getElementById('root')
 );

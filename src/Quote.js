07d6b8aa86f0fcda9f9a12c237997cd45ffc959a
Haya <hm.almadhi@gmail.com>
2021-02-23 13:59:27 +0300

    add button and change the view of quotes in quote component

--- src/Quote.js
+++ src/Quote.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 
 export default class Quote extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            likes: ''
+        }
+    }
     render() {
         return (
             <div>
-                {this.props.qbody}
-                <br />
+              Quote's title: {this.props.qtitle}  <br />  
+              《 &nbsp;  {this.props.qbody}  &nbsp;  》
+              <br />&nbsp; ــــــ {this.props.qfrom} <br />
+               By {this.props.qwriter}
+                <br /><button>Like it</button>
+                <br /><br />
                 <button onClick={() => { this.props.deleteQuote(this.props.id) }}>Delete "{this.props.qtitle}"</button>
+                &nbsp;
                 <button onClick={() => { this.props.editView(this.props.id) }}>Edit "{this.props.qtitle}"</button>
                 <hr />
             </div>

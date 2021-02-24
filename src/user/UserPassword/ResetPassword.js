dccb54d7f81bde7eaae77f61009c386c585162bc
Ghaidaa Hussain <gadhus@outlook.com>
2021-02-24 13:29:10 +0300

    commit

--- src/user/UserPassword/ResetPassword.js
+++ src/user/UserPassword/ResetPassword.js
@@ -59,7 +59,6 @@ export default class ResetPassword extends Component {
         (<Alert class="alert alert-warning alert-dismissible fade show" role="alert">
         <strong>{this.state.errors}</strong> 
          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
         </button>
     </Alert>) 
             : null;

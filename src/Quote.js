3578d8ef8e06abb7b791dcb2bb72921ebae376e4
Haya <hm.almadhi@gmail.com>
2021-02-24 14:19:35 +0300

    add container to quote div

--- src/Quote.js
+++ src/Quote.js
@@ -9,17 +9,18 @@ export default class Quote extends Component {
     }
     render() {
         return (
-            <div>
-              Quote's title: {this.props.qtitle}  <br />  
+            <div className="stdiv">
+              Quote's title: {this.props.qtitle}  <br />  <br/>
               《 &nbsp;  {this.props.qbody}  &nbsp;  》
-              <br />&nbsp; ــــــ {this.props.qfrom} <br />
-               By {this.props.qwriter}
+              <br /> <br/>&nbsp; ــــــ {this.props.qfrom} 
+              &nbsp; By {this.props.qwriter}
                 <br /><button>Like it</button>
                 <br /><br />
+                <hr />
                 <button onClick={() => { this.props.deleteQuote(this.props.id) }}>Delete "{this.props.qtitle}"</button>
                 &nbsp;
                 <button onClick={() => { this.props.editView(this.props.id) }}>Edit "{this.props.qtitle}"</button>
-                <hr />
+                
             </div>
         )
     }

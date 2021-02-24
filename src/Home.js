28e7d10c60465e8926dfb1623f6f2aaac2c40fbf
Ghaidaa Hussain <gadhus@outlook.com>
2021-02-24 18:59:57 +0300

    adding first var menu

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import "./Home.css";
 import axios from 'axios';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Quote from './Quote';
@@ -94,12 +95,16 @@ export default class Home extends Component {
             <div>
 
                 <Router>
-                    <nav>
-                        {/* <Link to="/">Home</Link>{' '} */}
-                        <Link to="/addquote">Add Quote</Link> {' '}
-                        <Link to="/allquote">All Quote</Link> {' '}
-                        {/* <Link to="/addquote">Edit Quote</Link> {' '} */}
-                    </nav>
+                <div class="menu">
+  <div class="label">Main Menu</div>
+  <div class="spacer"></div>
+  <div class="item"><span><Link to="/addquote">Add Quote</Link></span></div>
+  <div class="item"><span><Link to="/allquote">All Quote</Link></span></div>
+  <div class="item"><span>Flickr</span></div>
+  <div class="item"><span>Behance</span></div>
+  <div class="item"><span>MixCloud</span></div>
+</div>
+
                     <div>
                         {/* <Route exact path="/" component={Home}></Route> */}
                         <Route path="/addquote" component={() => <NewQuote addQuote={this.addQuote} />}></Route>

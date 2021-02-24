a234f233c3e21612920694febea21d3f161863c6
Ghaidaa Hussain <gadhus@outlook.com>
2021-02-24 12:55:50 +0300

    change  the style of the  resetpassword page

--- src/user/UserPassword/ResetPassword.js
+++ src/user/UserPassword/ResetPassword.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import axios from "axios";
 import { Container, Form, Button, Alert } from 'react-bootstrap'
+import TextField from '@material-ui/core/TextField';
+
 
 export default class ResetPassword extends Component {
     state = {
-        errors: ''
+        errors: '',
+        passwordfield: false,
+        comfpasswordfield: false
+
     }
     changeHandler = (e) => {
         let temp = { ...this.state }
@@ -12,19 +17,23 @@ export default class ResetPassword extends Component {
         this.setState(temp)
 
     }
+     
     checkPassword = () => {
         if (this.state.password === this.state.confpassword && (!!this.state.password  || !!this.state.confpassword )) {
             this.changepasswordHandler();
         } else if (!this.state.password  && !this.state.confpassword) {
             console.log("full the fields please");
             this.setState({
-                errors: 'Passwords fields should not be empty'
+                errors: 'Passwords fields should not be empty',
+                passwordfield: true,
+                comfpasswordfield: true
             })
         }
         else {
             console.log("don't match");
             this.setState({
-                errors: 'Passwords DO NOT match'
+                errors: 'Passwords DO NOT match',
+                comfpasswordfield: true
             })
         }
 
@@ -47,30 +56,48 @@ export default class ResetPassword extends Component {
 
     render() {
         const message = this.state.errors ? 
-        (<div class="alert alert-warning alert-dismissible fade show" role="alert">
+        (<Alert class="alert alert-warning alert-dismissible fade show" role="alert">
         <strong>{this.state.errors}</strong> 
          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
-    </div>) 
+    </Alert>) 
             : null;
+
         return (
             <div>
                 <Container >
                     <Form.Group>
-                        <Form.Label>New Password</Form.Label>
-                        <Form.Control type="password" name="password"
-
-                            placeholder="Enter your new password" onChange={this.changeHandler}></Form.Control>
+                        
+                        
+                             <TextField 
+                             fullWidth
+                             type="password"
+                        label=" New Password"
+                        name="password"
+                        error={this.state.passwordfield ? true : false }
+                        placeholder="Enter your new password"
+                        onChange={this.changeHandler}
+                        variant="filled"
+                        
+                      />
                     </Form.Group>
                     <Form.Group>
-                        <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control type="password" name="confpassword"
-                            placeholder="Confirm your new password" onChange={this.changeHandler}></Form.Control>
+                        <TextField 
+                        fullWidth
+                        label="Confirm Password"
+                        type='password'
+                        error={this.state.comfpasswordfield ? true : false }
+                        placeholder="Confirm your new password"
+                        onChange={this.changeHandler}
+                        variant="filled"
                         
-                        {message}
+                      />        
+                              
+                               {message}
 
                     </Form.Group>
+                    
                     <Button variant="primary" onClick={this.checkPassword}>Change</Button>
                 </Container>
 

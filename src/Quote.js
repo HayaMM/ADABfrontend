63c7ed2055afa47e111ab2db875ee2f73333cf08
Haya <hm.almadhi@gmail.com>
2021-03-01 12:07:27 +0300

    my quote style

--- src/Quote.js
+++ src/Quote.js
@@ -4,16 +4,16 @@ export default class Quote extends Component {
     render() {
         return (
             <div className="stdiv">
-              Quote's title: {this.props.qtitle}  <br />  <br/>
-              《 &nbsp;  {this.props.qbody}  &nbsp;  》
-              <br /> <br/>&nbsp; ــــــ {this.props.qfrom} 
+                <p className="h">Quote's title:&nbsp;<b>{this.props.qtitle}</b>    <br />  <br />
+                    <b> 《 &nbsp;  {this.props.qbody}  &nbsp;  》</b>
+                    <br /> <br />&nbsp; ــــــ {this.props.qfrom}
               &nbsp; By {this.props.qwriter}
-                <br /><br />
-                <hr />
-                <button onClick={() => { this.props.deleteQuote(this.props.id) }}>Delete "{this.props.qtitle}"</button>
+                    <br /><br />
+                    <hr />
+                    <button className="btn" variant="primary" onClick={() => { this.props.deleteQuote(this.props.id) }}>Delete "{this.props.qtitle}"</button>
                 &nbsp;
-                <button onClick={() => { this.props.editView(this.props.id) }}>Edit "{this.props.qtitle}"</button>
-                
+                <button className="btn" variant="primary" onClick={() => { this.props.editView(this.props.id) }}>Edit "{this.props.qtitle}"</button>
+                </p>
             </div>
         )
     }

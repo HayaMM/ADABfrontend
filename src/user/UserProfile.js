cf5624a765f2ee8578365ff096bd0b42c78b005b
Haya <hm.almadhi@gmail.com>
2021-03-01 12:40:25 +0300

    user profile style

--- src/user/UserProfile.js
+++ src/user/UserProfile.js
@@ -3,21 +3,23 @@ import Image from './Image'
 
 
 export default class UserProfile extends Component {
-    
+
     render() {
         return (
             <div className="stdiv">
-                {/* user_id:  {this.props.user.id} <br/> */}
-                First Name:  {this.props.user.firstName}<br/>
-                 Last Name: {this.props.user.lastName}<br/>
-                Email Addrees:  {this.props.user.emailAddress}<br/>
-                 Date Of Birth: {this.props.user.dateofBirth}<br/>
-                 profile image : <Image  theimage={this.props.user.image} id={this.props.user.id} loadUserProfile={this.props.loadUserProfile}/>
-                 <button onClick={()=>{this.props.editView(this.props.user.id)}}>Edit</button>
-                 {/* <button onClick={()=>{this.props.deleteAccount(this.props.id)}}>Delete account</button> */}
+                <div className="h">
+                    <h2 className="h">My Information</h2>
+                    {/* user_id:  {this.props.user.id} <br/> */}
+                First Name:  <b>{this.props.user.firstName}</b><br />
+                 Last Name: <b>{this.props.user.lastName}</b><br />
+                Email Addrees: <b> {this.props.user.emailAddress}</b><br />
+                 Date Of Birth:<b> {this.props.user.dateofBirth}</b><br />
+                 profile image :   <br /> <br /> <Image theimage={this.props.user.image} id={this.props.user.id} loadUserProfile={this.props.loadUserProfile} />
+                    <br />  <button className="btn" variant="primary" onClick={() => { this.props.editView(this.props.user.id) }}>Edit</button>
+                    {/* <button onClick={()=>{this.props.deleteAccount(this.props.id)}}>Delete account</button> */}
 
-               
-                
+                </div>
+
             </div>
         )
     }

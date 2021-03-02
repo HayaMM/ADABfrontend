a97b41b82ebe819b13a25d7a56c4456ee95f13c0
Haya <hm.almadhi@gmail.com>
2021-03-02 16:06:05 +0300

    video tag works

--- src/Video.js
+++ src/Video.js
@@ -4,10 +4,11 @@ export default function Video() {
     return (
         <div className="stdiv">
             <h1 className="h">About Us</h1><br />
-            <iframe width="500" height="315" src="https://www.youtube.com/embed/P7sbrLgMPpk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            {/* <video width="400" controls>
+            {/* <iframe width="500" height="315" src="https://www.youtube.com/embed/P7sbrLgMPpk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    */}
+            <video width="400" controls>
                 <source src={ADABapp} type="video/mp4" />
-            </video> */}
+            </video>
         </div>
     )
 }
